3b0701d8b3acd366550642a70e66c2c9d454fa9a
Levent Ali <lebreeze@gmail.com>
2011-11-17 20:22:58 +0000

    Initial Commit

---
+++ PachStatX/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  PachStatX
+//
+//  Created by Levent Ali on 17/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ PachStatX/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PachStatX
+//
+//  Created by Levent Ali on 17/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
