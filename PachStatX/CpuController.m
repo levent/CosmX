a573aaf2d4c6b3d132a812a6e67b32d48a106190
Levent Ali <lebreeze@gmail.com>
2011-11-18 17:30:21 +0000

    Memory alternatives commented

--- PachStatX/CpuController.m
+++ PachStatX/CpuController.m
@@ -43,6 +43,34 @@
     sizeRam = sizeof(info);
     kern_return_t kerr = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&info, &sizeRam);
 
+    
+    
+    
+    
+//    mach_port_t host_port;
+//    mach_msg_type_number_t host_size;
+//    vm_size_t pagesize;
+//    
+//    
+//    host_port = mach_host_self();
+//    host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+//    host_page_size(host_port, &pagesize);        
+//    
+//    vm_statistics_data_t vm_stat;
+//    
+//    if (host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size) != KERN_SUCCESS)
+//        NSLog(@"Failed to fetch vm statistics");
+//    
+//    /* Stats in bytes */ 
+//    natural_t mem_used = (vm_stat.active_count +
+//                          vm_stat.inactive_count +
+//                          vm_stat.wire_count) * pagesize;
+//    natural_t mem_free = vm_stat.free_count * pagesize;
+//    natural_t mem_total = mem_used + mem_free;
+//    NSLog(@"used: %u free: %u total: %u", mem_used, mem_free, mem_total);
+//    
+    
+    
 
     if(err == KERN_SUCCESS) {
         [CPUUsageLock lock];
