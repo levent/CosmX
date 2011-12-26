f4ae9a76a751c1c7f22319b2d933715cb7c1080d
Levent Ali <lebreeze@gmail.com>
2011-12-26 11:20:59 +0000

    Handle standard keyboard shortcuts

---
+++ PachStatX/ApplicationTemplate.h
@@ -0,0 +1,13 @@
+//
+//  ApplicationTemplate.h
+//  PachStatX
+//
+//  Created by Levent Ali on 26/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface ApplicationTemplate : NSApplication
+
+@end
